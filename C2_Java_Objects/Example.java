b5bd8b0ce9801047239d5cdfc98db1eca826c53c
syoustra <steph.you07@gmail.com>
2017-11-10 19:12:23 -0500

    Learning Constants

--- C2_Java_Objects/Example.java
+++ C2_Java_Objects/Example.java
@@ -3,6 +3,8 @@ public class Example {
   public static void main(String[] args) {
     // Your amazing code goes here...
     System.out.println("We are making a new Pez dispenser");
+    System.out.printf("FUN FACT: There are %d PEZ allowed in every dispenser %n",
+                      PezDispenser.MAX_PEZ);
     PezDispenser dispenser = new PezDispenser("Yoda");
     System.out.printf("The dispenser is %s %n",
                       dispenser.getCharacterName()
