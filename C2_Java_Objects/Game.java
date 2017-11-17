54e35553ca648c6dab7b10d7833d982bd7a1100e
syoustra <steph.you07@gmail.com>
2017-11-17 01:35:18 -0500

    Storing guesses in hits/misses string

--- C2_Java_Objects/Game.java
+++ C2_Java_Objects/Game.java
@@ -1,7 +1,21 @@
 class Game {
 	private String answer;
+	private String hits;
+	private String misses;
 
 	public Game(String answer) {
 		this.answer = answer;
+		hits = "";
+		misses = ""; 
+	}
+
+	public boolean applyGuess(char letter) {
+		boolean isHit = answer.indexOf(letter) != -1;
+		if (isHit) {
+			hits += letter;
+		} else {
+			misses += letter;
+		}
+		return isHit;
 	}
 }
