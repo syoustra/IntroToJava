ab05d4e793c59b10544d23d334f30bd17f22ea31
syoustra <steph.you07@gmail.com>
2017-11-17 02:03:45 -0500

    Prompting for guesses .... and renaming/capitalizing Prompter file

--- C2_Java_Objects/Hangman.java
+++ C2_Java_Objects/Hangman.java
@@ -3,6 +3,12 @@ public class Hangman {
   public static void main(String[] args) {
     // Your incredible code goes here...
     Game game = new Game("treehouse");
-
+    Prompter prompter = new Prompter(game);
+    boolean isHit = prompter.promptForGuess();
+    if (isHit) {
+    	System.out.println("We got a hit!");
+    } else {
+    	System.out.println("Oops, missed");
+    }
   }
 }
--- C2_Java_Objects/prompter.java
+++ C2_Java_Objects/prompter.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 class Prompter {
-	
+	private Game game;
+
+	public Prompter(Game game) {
+		this.game = game;
+	}
+
+	public boolean promptForGuess() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a letter:  ");
+		String guessInput = scanner.nextLine();
+		char guess = guessInput.charAt(0);
+		return game.applyGuess(guess);
+	}
 }
