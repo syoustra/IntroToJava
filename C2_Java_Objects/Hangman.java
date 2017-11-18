8f51121704bd983c781078adf51f5ebdef724c80
syoustra <steph.you07@gmail.com>
2017-11-18 13:00:07 -0500

    Adding some blank lines so the asker can scroll to prevent the guesser from seeing the submitted answer

--- C2_Java_Objects/Hangman.java
+++ C2_Java_Objects/Hangman.java
@@ -6,8 +6,12 @@ public class Hangman {
   		System.out.println("Usage:  java Hangman <answer>");
   		System.err.println("answer is required");
   		System.exit(1);
-  	}
-
+  	} 
+  // MY CODE TO GIVE SOME SPACE BETWEEN ANSWER AND GUESSES
+      else {
+        System.out.printf("%n %n %n %n %n %n %n %n %n %n");
+      }
+  // END MY ADD-ON CODE
     Game game = new Game(args[0]);
     Prompter prompter = new Prompter(game);
     while (game.getRemainingTries() > 0 && !game.isWon()) {
