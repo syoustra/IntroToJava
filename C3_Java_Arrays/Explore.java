888f1ba20f481569461f5b8bd49765d0c29742fe
syoustra <steph.you07@gmail.com>
2017-11-25 17:23:31 -0500

    Multidimensional arrays

--- C3_Java_Arrays/Explore.java
+++ C3_Java_Arrays/Explore.java
@@ -4,25 +4,49 @@ public class Explore {
 		// Create new friends array using the array literal shortcut.
 		// Include Ben, Alena, Pasan
 		String[] friends = {"Ben", "Alena", "Pasan"};
-		// Enhanced for loop
-		for (String friend : friends) {
-			System.out.printf("Hey %s! This movie starts at 7, C U there! %n",
-				friend);
+
+		int[][] scoreCards = {
+			// Ben's
+			{1, 2, 4, 2, 6, 5, 4, 3, 3, 2, 5, 7, 2, 7, 8, 4, 3, 2},
+			// Alena's
+			{2, 3, 5, 1, 1, 2, 3, 1, 1, 2, 4, 1, 3, 3, 2, 6, 3, 2},
+			// Pasan's
+			{4, 4, 2, 1, 2, 2, 1, 4, 2, 2, 2, 3, 2, 5, 8, 1, 2, 2}
+		};
+
+		// for each friend
+		for (int i = 0; i < friends.length; i++) {
+		//  print their name
+			System.out.printf("%s %n -------------------%n",
+				friends[i]);
+		//   for each hole
+			for (int j = 0; j < scoreCards[i].length; j++) {
+		//    print their score
+				System.out.printf("Hole %d: %d %n",
+					j + 1,
+					scoreCards[i][j]);					
+			}
 		}
+
+		// Enhanced for loop
+		// for (String friend : friends) {
+		// 	System.out.printf("Hey %s! This movie starts at 7, C U there! %n",
+		// 		friend);
+		// }
 
 		// Basic for loop
-		for (int i = 0; i < friends.length; i++) {
-			String friend = friends[i];
-			System.out.printf("Hey %s! This movie starts at 7, C U there! %n",
-				friend);
-		}
+		// for (int i = 0; i < friends.length; i++) {
+		// 	String friend = friends[i];
+		// 	System.out.printf("Hey %s! This movie starts at 7, C U there! %n",
+		// 		friend);
+		// }
 
-		int[] bensScoreCard = {1, 2, 1, 5, 2, 4, 4, 4, 3, 6, 1, 2, 5, 4, 3, 2, 6, 3};
-		for (int i = 0; i < bensScoreCard.length; i++) {
-			System.out.printf("Hole #%d: %d %n",
-				i + 1,
-				bensScoreCard[i]);
-		}
+		// int[] bensScoreCard = {1, 2, 1, 5, 2, 4, 4, 4, 3, 6, 1, 2, 5, 4, 3, 2, 6, 3};
+		// for (int i = 0; i < bensScoreCard.length; i++) {
+		// 	System.out.printf("Hole #%d: %d %n",
+		// 		i + 1,
+		// 		bensScoreCard[i]);
+		// }
 	}
 
 }
