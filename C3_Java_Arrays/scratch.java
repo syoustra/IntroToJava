d96a444e9084a0536711fe22760ba7927968c239
syoustra <steph.you07@gmail.com>
2017-11-26 02:01:00 -0500

    Using scratch sandbox for sorting

--- C3_Java_Arrays/scratch.java
+++ C3_Java_Arrays/scratch.java
@@ -1,10 +1,11 @@
-int[][] scoreCards = {
-	// Ben's
-	{1, 2, 4, 2, 6, 5, 4, 3, 3, 2, 5, 7, 2, 7, 8, 4, 3, 2}
-	// Alena's
-	{2, 3, 5, 1, 1, 2, 3, 1, 1, 2, 4, 1, 3, 3, 2, 6, 3, 2}
-	// Pasan's
-	{4, 4, 2, 1, 2, 2, 1, 4, 2, 2, 2, 3, 2, 5, 8, 1, 2, 2}
+import java.util.Arrays;
+import java.util.Comparator;
+
+String[] friends = {
+	"Treasure",
+	"Ben",
+	"Alena",
+	"Pasan",
+	"Craig"
 };
-System.out.printf("Pasan, 15th hole, %d %n",
-	scoreCards[2][14]);
+
