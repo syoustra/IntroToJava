3ad8b10b23f6b2f07991b2094399378d8c877f6f
syoustra <steph.you07@gmail.com>
2017-11-26 02:13:19 -0500

    Using scratch sandbox to try var args

--- C3_Java_Arrays/scratch.java
+++ C3_Java_Arrays/scratch.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Random;
 
 String[] friends = {
 	"Treasure",
@@ -9,3 +10,13 @@ String[] friends = {
 	"Craig"
 };
 
+
+public String pickLunchSpot(String... spots) {
+	System.out.printf("Randomly picking %d lunch spots. %n",
+		spots.length);
+	if (spots.length == 0) {
+		return "Someplace with tacos";
+	}
+	Random random = new Random();
+	return spots[random.nextInt(spots.length)];
+}
